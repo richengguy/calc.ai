fd70d76e6de1e5c7ae6aa684e117f27d89901b4e
Richard Rzeszutek <richengguy@users.noreply.github.com>
2025-03-22 22:22:06 -0400

    Added the position encoding.

--- src/calcai/model/layers.py
+++ src/calcai/model/layers.py
@@ -1,3 +1,6 @@
+import math
+
+import torch
 from torch import Tensor
 from torch.nn import Linear, Module
 from torch.nn import functional as F
@@ -25,9 +28,18 @@ class TokenEmbedding(Module):
             )
 
         self._vocab_size = vocab_size
+        self._num_dim = num_dim
 
         self._linear = Linear(vocab_size, num_dim)
         kaiming_uniform_(self._linear.weight, nonlinearity="relu")
+
+    @property
+    def vocab_size(self) -> int:
+        return self._vocab_size
+
+    @property
+    def num_dim(self) -> int:
+        return self._num_dim
 
     def forward(self, tokens: Tensor) -> Tensor:
         """Project the token sequence into the embedding space.
@@ -45,3 +57,79 @@ class TokenEmbedding(Module):
         one_hot = F.one_hot(tokens.long(), self._vocab_size).float()
         projected = self._linear(one_hot)
         return projected
+
+
+class PositionEncoding(Module):
+    """Applies a position encoding to an input tensor.
+
+    This uses the same sine/cosine encoding scheme from the original "Attention
+    is all you need" paper.  This creates a unique sinusoid for each token
+    position.
+
+    A more detailed explanation on how this position encoding works can be found
+    in https://machinelearningmastery.com/a-gentle-introduction-to-positional-encoding-in-transformer-models-part-1/
+    """  # noqa: E501
+
+    def __init__(self, num_dim: int, n: float = 10000) -> None:
+        """
+        Parameters
+        ----------
+        num_dim : int
+            the dimensionality of the embedding space; this must be an even
+            number
+        n : int
+            the scalar value that controls the "base" sinusoid frequency;
+            defaults to 10'000 since this is what was used in the original
+            paper
+        """
+        if num_dim % 2 != 0:
+            # This isn't strictly necessary but odd-valued embedding spaces make
+            # the indexing math a bit messy.
+            raise ValueError("Input dimensionality must be an even value.")
+
+        denom_size = math.ceil(num_dim / 2)
+        self._denom = torch.pow(n, 2 * torch.arange(0, denom_size) / num_dim)
+        self._num_dim = num_dim
+        self._n = n
+
+    def forward(self, x: Tensor) -> Tensor:
+        """Apply the position encoding.
+
+        Parameters
+        ----------
+        x : Tensor
+            input tensor shaped `(B, N_tokens, N_dim)`
+
+        Returns
+        -------
+        Tensor
+            the tensor with the position encoding applied
+        """
+        if x.ndim != 3:
+            raise RuntimeError("Tensor must only have three dimensions.")
+
+        if x.shape[2] != self._num_dim:
+            raise RuntimeError(
+                f"The last dimension in the input tensor was {x.shape[-1]}, expected {self._num_dim}."
+            )
+
+        num_tokens = x.shape[1]
+        size_denom = len(self._denom)
+
+        # This does some resizing so that the indices and denominator vectors
+        # are the same size.  This is done using views so no new memory should
+        # be allocated
+        inds = (
+            torch.arange(num_tokens).reshape((num_tokens, 1)).expand((-1, size_denom))
+        )
+        denom = self._denom.expand((num_tokens, -1))
+
+        # This then computes the odd and even indices of the position vector.
+        odd_dim = torch.sin(inds / denom)
+        evn_dim = torch.cos(inds / denom)
+
+        encoding = torch.zeros_like(x)
+        encoding[:, :, 0::2] = odd_dim
+        encoding[:, :, 1::2] = evn_dim
+
+        return x + encoding
--- test/test_layers.py
+++ test/test_layers.py
@@ -1,4 +1,5 @@
 import pytest
+import torch
 from torch import Tensor
 
 from calcai.model import layers
@@ -12,3 +13,19 @@ def test_embedding_layer(vocab_size: int, num_tokens: int, num_dim: int) -> None
     embedding = layers.TokenEmbedding(vocab_size, num_dim)
     output = embedding.forward(input)
     assert output.shape == (num_tokens, num_dim)
+
+
+@pytest.mark.parametrize("num_tokens", (2, 3, 4, 5))
+@pytest.mark.parametrize("num_dim", (4, 6, 8, 10))
+def test_position_encoding(num_tokens: int, num_dim: int) -> None:
+    """Verify the position encoding is applied correctly."""
+    input = torch.zeros((2, num_tokens, num_dim))
+    encoding = layers.PositionEncoding(num_dim)
+    output = encoding.forward(input)
+    assert output.shape == input.shape
+
+
+def test_error_on_odd_embedding_size_for_position_encoding() -> None:
+    """Raise an exception if the embedding space has an odd number of dimensions."""
+    with pytest.raises(ValueError):
+        layers.PositionEncoding(3)
