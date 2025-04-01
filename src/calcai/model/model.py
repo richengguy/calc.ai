8d62fd2a9aefeed5789f125ebe5bf81fc46b4bb4
Richard Rzeszutek <richengguy@users.noreply.github.com>
2025-03-31 21:52:55 -0400

    Started on the actual wrapper object.

---
+++ src/calcai/model/model.py
@@ -0,0 +1,19 @@
+from .layers import SimpleDecoderTransformer
+from .tokenizer import ControlToken, Tokenizer
+
+
+class CalculatorLanguageModel:
+    """A simple transformer for computing arithmetic expressions.
+
+    The Calculator Language Model (CLM) will accept a string, something like
+    `1 + 2`, and, in theory, generate `3`.
+    """
+    def __init__(self, *, embedding_dimensions: int = 16) -> None:
+        """
+        Parameters
+        ----------
+        embedding_dimensions : int
+            size of the token embedding space; defaults to 16
+        """
+        self._tokenizer = Tokenizer()
+        self._model = SimpleDecoderTransformer(self._tokenizer.num_tokens, embedding_dimensions)
