ff1129d252654b41afd985afab752a2fbbe98514
Richard Rzeszutek <richengguy@users.noreply.github.com>
2025-04-05 15:30:30 -0400

    Added the training step.

--- src/calcai/model/model.py
+++ src/calcai/model/model.py
@@ -55,6 +55,11 @@ class CalculatorLanguageModel:
         self._max_context = max_context
         self._context: list[int] = []
 
+    @property
+    def max_context_size(self) -> int:
+        """Maximum context window size."""
+        return self._max_context
+
     def predict(self, query: str) -> Iterator[str]:
         """Predict tokens, given some input.
 
@@ -86,6 +91,41 @@ class CalculatorLanguageModel:
                 # Stop processing if we see a "result end" token.
                 if token == ControlToken.RESULT_STOP:
                     break
+
+    def training_step(self, input: str, *, init: bool = False) -> Tensor:
+        """Perform a single training iteration.
+
+        Parameters
+        ----------
+        input : str
+            input string
+        init : bool
+            if ``True`` then this resets the model's context window to indicate
+            the start of a new training sequence
+
+        Returns
+        -------
+        Tensor
+            the model's next predicted token
+
+        Raises
+        ------
+        RuntimeError
+            if the maximum allowable context window size is exceeded
+        """
+        if init:
+            self._context = list(self._tokenizer.to_tokens(input))
+        else:
+            self._context.extend(self._tokenizer.to_tokens(input))
+
+        if len(self._context) > self._max_context:
+            raise RuntimeError("Exceeded the maximum allowed context size.")
+
+        if not self._model.training:
+            self._model.train()
+
+        tokens = Tensor(self._context)
+        return self._model(tokens[torch.newaxis, :])
 
     def reset(self) -> None:
         """Resets the model's internal state."""
